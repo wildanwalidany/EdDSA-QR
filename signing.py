dba1c1cce80728800c72078da663168cf42ad636
wildanwalidany <wildanwalidany21@gmail.com>
2022-12-11 17:29:39 +0700

    clean

--- signing.py
+++ signing.py
@@ -3,19 +3,18 @@ from nacl.signing import SigningKey
         #SIGNING
 
 # Generate a new random signing key                                 PRIVATE KEY
-signing_key = SigningKey.generate()
-with open("signing_key.bin", "wb") as f:
-    signing_key.save(f)
-    
+private_key = SigningKey.generate()
+
 # Sign a message with the signing key                               SIGNATURE
-signed = signing_key.sign(b"Attack at Dawn")
+signature = private_key.sign(b"Ini Pesan")
+
 print("SIGNATURE")
-print(signed)
+print(signature)
 
 # Obtain the verify key for a given signing key                     PUBLIC KEY
-verify_key = signing_key.verify_key
+public_key = private_key.verify_key
 
 # Serialize the verify key to send it to a third party              PUBLIC KEY ENCODED
-verify_key_bytes = verify_key.encode()
+public_key_bytes = public_key.encode()
 print("PUBLIC KEY")
-print(verify_key_bytes)
+print(public_key_bytes)
