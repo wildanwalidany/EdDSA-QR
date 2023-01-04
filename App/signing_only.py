993f604ea49dd7e24379852d9f9d50441027421a
wildanwalidany <wildanwalidany21@gmail.com>
2023-01-04 20:17:41 +0700

    refactor

--- App/signing_only.py
+++ App/signing_only.py
@@ -1,7 +1,8 @@
+from json_generator import JsonGen
+from qr_encoder import QREncode
 from sign_generator import SignGenerator
 from user import User
-from json_generator import JsonGEn
-from qr_encoder import QREncode
+
 
 def main():
     '============= Sign Up, Login ============='
@@ -15,14 +16,14 @@ def main():
     print("PRIVATE", private_key, "PUBLIC", public_key)
 
     '============= Identity ============='
-    new_json = JsonGEn(name, 'pengesahan proposal PA', 'jane foster')
+    new_json = JsonGen(name, 'pengesahan proposal PA', 'jane foster')
     identity = new_json.to_json_bytes()
     print('IDENTITY', identity)
     
     '============= SIGNING ============='
     # Create new sign object
     new_sign = SignGenerator(private_key, identity)
-    signed_message = new_sign.get_signature()
+    signed_message = new_sign.get_signed()
 
     print('message',signed_message.message)
     print('len',len(signed_message.message))
@@ -34,7 +35,7 @@ def main():
     '============= ENCODE =============' 
     new_qr = QREncode(signed_message)
 
-    print("jalan coy")
+    print("jalan coy ✅✅✅")
     
 if __name__ == "__main__":
     main()
