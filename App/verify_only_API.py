6af72e8d24d1ebf4456962ca2f646e6eb9cc0e4f
wildanwalidany <wildanwalidany21@gmail.com>
2023-01-05 13:38:51 +0700

    add api

---
+++ App/verify_only_API.py
@@ -0,0 +1,51 @@
+import json
+
+from api_find import APIFind
+from nacl.encoding import Base64Encoder
+from qr_reader import Reader
+from verifier import Verifier
+
+
+def main():
+
+    '============= VERIFY ============='
+    # Read QR Code
+    new_read = Reader()
+    data = new_read.read()
+    print('data read', data)
+
+    # Decode the signed message
+    signed_bytes = Base64Encoder.decode(data)
+
+
+    # Extract the message
+    word = b'{"name"'
+    start_index = signed_bytes.index(word)
+    rest_of_bytes = signed_bytes[start_index:]
+    print(rest_of_bytes)  
+    
+    # Parse the string as a JSON object
+    json_obj = json.loads(rest_of_bytes)
+
+    # Access the "name" field
+    name = json_obj["name"]
+
+    print('name', name) 
+
+    # Find(identity)
+    new_find = APIFind(name)
+
+    # Get(public_key)
+    public_key = new_find.getKey()
+    print('public_key', public_key)
+
+    # Create new verify object
+    
+    new_verify = Verifier(public_key, data)
+    result = new_verify.result()
+    print('RESULT', result)
+
+    print("✅✅✅")
+    
+if __name__ == "__main__":
+    main()
