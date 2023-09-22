22b32132fb14f725865950843557c14176c52136
wildanwalidany <wildanwalidany21@gmail.com>
2023-09-22 19:39:26 +0700

    Add error handling

--- App/user.py
+++ App/user.py
@@ -8,78 +8,96 @@ class User:
         self.password = password
     
     def sign_up(self):
-        # Hash the password
-        hashed_password = hashlib.sha256(self.password.encode()).hexdigest()
+        try:
+            # Hash the password
+            hashed_password = hashlib.sha256(self.password.encode()).hexdigest()
 
-        # Load URL
-        MONGODB_URL = "mongodb+srv://wildanwalidany21:Kenshin77@cluster0.psea199.mongodb.net/?retryWrites=true&w=majority"
-        # Connect to the database
-        client = MongoClient(MONGODB_URL)
-        db = client['test_db']
-        # Define keys collection
-        keys_collection = db['keys']
+            # Load URL
+            MONGODB_URL = "mongodb+srv://wildanwalidany21:Kenshin77@cluster0.psea199.mongodb.net/?retryWrites=true&w=majority"
 
-        # Create a new KeyGenerator object
-        key_generator = KeyGenerator()
+            # Connect to the database
+            client = MongoClient(MONGODB_URL)
+            db = client['test_db']
 
-        # Get the generated private and public keys
-        self.private_key, self.public_key   = key_generator.get_binary_keys()
+            # Define keys collection
+            keys_collection = db['keys']
 
-        # Create new keys document
-        new_keys = {
-            'username': self.username,
-            "private_key": self.private_key,
-            "public_key": self.public_key,
-        }
+            # Create a new KeyGenerator object
+            key_generator = KeyGenerator()
 
-        # Store the keys in the database
-        result1 = keys_collection.insert_one(new_keys)
-        
-        # Define user's credentials in the database
-        users_collection = db['users']
+            # Get the generated private and public keys
+            self.private_key, self.public_key = key_generator.get_binary_keys()
 
-        # Create new user object
-        new_users = {
-            'username': self.username,
-            'password': hashed_password,
-        }
+            # Create new keys document
+            new_keys = {
+                'username': self.username,
+                "private_key": self.private_key,
+                "public_key": self.public_key,
+            }
 
-        # Store the user's credentials in the database
-        result2 = users_collection.insert_one(new_users)
+            # Store the keys in the database
+            result1 = keys_collection.insert_one(new_keys)
 
-        # Create result object
-        document_id1 = result1.inserted_id
-        document_id2 = result2.inserted_id
-        print(f"_id of inserted keys document: {document_id1}")
-        print(f"_id of inserted users document: {document_id2}")
-        print('sign up success')
-        client.close()
-        # Return the keys
-        return self.private_key, self.public_key
+            # Define user's credentials in the database
+            users_collection = db['users']
+
+            # Create new user object
+            new_users = {
+                'username': self.username,
+                'password': hashed_password,
+            }
+
+            # Store the user's credentials in the database
+            result2 = users_collection.insert_one(new_users)
+
+            # Check if both inserts were successful
+            if result1.inserted_id and result2.inserted_id:
+                document_id1 = result1.inserted_id
+                document_id2 = result2.inserted_id
+                print(f"_id of inserted keys document: {document_id1}")
+                print(f"_id of inserted users document: {document_id2}")
+                print('sign up success')
+            else:
+                print('Sign up failed. Please try again.')
+            # Return the keys
+            return self.private_key, self.public_key
+        except Exception as e:
+            print(f'An error occurred during registration: {e}')
+        finally:
+            client.close()
 
     def login(self):
-        # Hash the password
-        hashed_password = hashlib.sha256(self.password.encode()).hexdigest()
-        # Load URL
-        MONGODB_URL = "mongodb+srv://wildanwalidany21:Kenshin77@cluster0.psea199.mongodb.net/?retryWrites=true&w=majority"
-        # Connect to the database
-        client = MongoClient(MONGODB_URL)
-        db = client['test_db']
+        try:
+            # Hash the password
+            hashed_password = hashlib.sha256(self.password.encode()).hexdigest()
+            # Load URL
+            MONGODB_URL = "mongodb+srv://wildanwalidany21:Kenshin77@cluster0.psea199.mongodb.net/?retryWrites=true&w=majority"
+            # Connect to the database
+            client = MongoClient(MONGODB_URL)
+            db = client['test_db']
 
-        # Verify the user's credentials
-        users_collection = db['users']
-        user = users_collection.find_one({'username': self.username, 'password': hashed_password})
-        if user is None:
-            return None
-        # Retrieve the user's signing and verify keys from the database
-        keys_collection = db['keys']
-        keys = keys_collection.find_one({'username': self.username})
-        private_key = keys['private_key']
-        public_key = keys['public_key']
-        print('login success')
-        client.close()
-        # Return the keys
-        return private_key, public_key
+            # Verify the user's credentials
+            users_collection = db['users']
+            user = users_collection.find_one({'username': self.username, 'password': hashed_password})
+
+            if user is None:
+                print('Login failed. Invalid credentials.')
+                return None
+
+            # Retrieve the user's signing and verify keys from the database
+            keys_collection = db['keys']
+            keys = keys_collection.find_one({'username': self.username})
+            private_key = keys['private_key']
+            public_key = keys['public_key']
+
+            print('Login success')
+            return private_key, public_key
+
+        except Exception as e:
+            print(f'An error occurred during login: {e}')
+        finally:
+            client.close()
+
 
 
 
