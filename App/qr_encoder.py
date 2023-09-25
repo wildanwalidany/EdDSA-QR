2d2140738f4a93a68e879f6c90f4b76c320a5e9d
wildanwalidany <wildanwalidany21@gmail.com>
2023-09-25 10:49:19 +0700

    Add error handling

--- App/qr_encoder.py
+++ App/qr_encoder.py
@@ -1,19 +1,24 @@
 import qrcode
+import traceback  # Import the traceback module
 
 class QREncode:
     def __init__(self, encoded_signature):
-        # Generate a QR code from the encoded signature
-        self.qr = qrcode.QRCode(
-            version=None,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        self.qr.add_data(encoded_signature)
-        self.qr.make(fit=True)
+        try:
+            # Generate a QR code from the encoded signature
+            self.qr = qrcode.QRCode(
+                version=None,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            self.qr.add_data(encoded_signature)
+            self.qr.make(fit=True)
 
-        # Save the QR code as an image
-        self.img = self.qr.make_image(fill_color="black", back_color="white")
-        self.img.save("./App/qrcode.png")
+            # Save the QR code as an image
+            self.img = self.qr.make_image(fill_color="black", back_color="white")
+            self.img.save("./App/qrcode.png")
 
-        return None
+        except Exception as e:
+            # Handle any exceptions that occur during QR code generation
+            print(f"QR code generation error: {e}")
+            traceback.print_exc()  # Print the full exception traceback for debugging
